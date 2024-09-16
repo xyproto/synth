093f9846ee91ab25f199d619d6e67817fee183ef
Alexander F. Rødseth <rodseth@gmail.com>
2024-09-16 15:33:52 +0200

    Update dependencies

--- vendor/github.com/AllenDang/cimgui-go/thirdparty.go
+++
@@ -1,9 +0,0 @@
-//go:build required
-// +build required
-
-package imgui
-
-import (
-	"github.com/AllenDang/cimgui-go/thirdparty/SDL/include"
-	"github.com/AllenDang/cimgui-go/thirdparty/glfw/include/GLFW"
-)
