3d6eaed7265d3a9935684fe435da9cfd9cf82bd2
Alexander F. Rødseth <rodseth@gmail.com>
2024-09-17 11:25:44 +0200

    Play audio directly with SDL2

--- wav.go
+++ wav.go
@@ -1,7 +1,8 @@
 package synth
 
 import (
-	"errors"
+	"bytes"
+	"encoding/binary"
 	"fmt"
 	"io"
 	"math"
@@ -11,6 +12,8 @@ import (
 
 	"github.com/go-audio/audio"
 	"github.com/go-audio/wav"
+	"github.com/veandco/go-sdl2/mix"
+	"github.com/veandco/go-sdl2/sdl"
 )
 
 // SaveToWav saves the waveform to a .wav file using int16 PCM format.
@@ -92,7 +95,7 @@ func FFPlayWav(filePath string) error {
 	return cmd.Wait()
 }
 
-// SaveKickTo generates a kick samples and saves it to a specified directory, avoiding filename collisions.
+// SaveKickTo generates kick samples and saves it to a specified directory, avoiding filename collisions.
 func (cfg *Settings) SaveKickTo(directory string) (string, error) {
 	n := 1
 	var fileName string
@@ -132,13 +135,93 @@ func (cfg *Settings) PlayKick() error {
 	return PlayWaveform(samples, cfg.SampleRate)
 }
 
-// PlayWav plays a WAV file
+// PlayWav plays a WAV file using SDL2 and SDL_mixer
 func PlayWav(filePath string) error {
-	// TODO: TO IMPLEMENT!
-	return errors.New("PlayWav: not implemented!")
+	// Initialize SDL
+	if err := sdl.Init(sdl.INIT_AUDIO); err != nil {
+		return fmt.Errorf("could not initialize SDL: %v", err)
+	}
+	defer sdl.Quit()
+
+	// Open the audio device
+	if err := mix.OpenAudio(44100, sdl.AUDIO_S16SYS, 2, 1024); err != nil {
+		return fmt.Errorf("could not open audio: %v", err)
+	}
+	defer mix.CloseAudio()
+
+	// Load the WAV file
+	music, err := mix.LoadMUS(filePath)
+	if err != nil {
+		return fmt.Errorf("could not load music file: %v", err)
+	}
+	defer music.Free()
+
+	// Play the music
+	if err := music.Play(1); err != nil {
+		return fmt.Errorf("could not play music: %v", err)
+	}
+
+	// Wait until the music is finished playing
+	for mix.PlayingMusic() {
+		sdl.Delay(100)
+	}
+
+	return nil
 }
 
+// PlayWaveform plays raw waveform samples using SDL2
 func PlayWaveform(samples []float64, sampleRate int) error {
-	// TODO: TO IMPLEMENT!
-	return errors.New("PlayWaveform: not implemented!")
+	// Initialize SDL
+	if err := sdl.Init(sdl.INIT_AUDIO); err != nil {
+		return fmt.Errorf("could not initialize SDL: %v", err)
+	}
+	defer sdl.Quit()
+
+	var desired, obtained sdl.AudioSpec
+
+	desired.Freq = int32(sampleRate)
+	desired.Format = sdl.AUDIO_F32SYS // 32-bit float samples
+	desired.Channels = 1              // Mono
+	desired.Samples = 4096            // Buffer size
+
+	// Open the audio device
+	audioDeviceID, err := sdl.OpenAudioDevice("", false, &desired, &obtained, 0)
+	if err != nil {
+		return fmt.Errorf("could not open audio device: %v", err)
+	}
+	defer sdl.CloseAudioDevice(audioDeviceID)
+
+	// Check if obtained spec matches desired spec
+	if obtained.Format != desired.Format || obtained.Channels != desired.Channels || obtained.Freq != desired.Freq {
+		return fmt.Errorf("obtained audio spec does not match desired spec")
+	}
+
+	// Convert samples []float64 to []float32
+	audioData := make([]float32, len(samples))
+	for i, sample := range samples {
+		audioData[i] = float32(sample)
+	}
+
+	// Convert audioData []float32 to []byte
+	buf := new(bytes.Buffer)
+	for _, f := range audioData {
+		if err := binary.Write(buf, binary.LittleEndian, f); err != nil {
+			return fmt.Errorf("error converting float32 to bytes: %v", err)
+		}
+	}
+
+	// Queue the audio data
+	if err := sdl.QueueAudio(audioDeviceID, buf.Bytes()); err != nil {
+		return fmt.Errorf("could not queue audio data: %v", err)
+	}
+
+	// Start playback
+	sdl.PauseAudioDevice(audioDeviceID, false)
+
+	// Wait until the audio finishes playing
+	for sdl.GetQueuedAudioSize(audioDeviceID) > 0 {
+		sdl.Delay(100)
+	}
+
+	return nil
 }
