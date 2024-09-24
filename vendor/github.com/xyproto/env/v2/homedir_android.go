dc24cf1300109d9c4c3919d34d66a132e82377d2
Alexander F. Rødseth <rodseth@gmail.com>
2024-09-24 17:08:06 +0200

    Update dependencies

--- vendor/github.com/xyproto/env/v2/homedir_android.go
+++
@@ -1,8 +0,0 @@
-//go:build android
-
-package env
-
-// userHomeDir returns the same value as os.UserHomeDir for Android
-func userHomeDir() (string, error) {
-	return "/sdcard", nil
-}
--- vendor/github.com/xyproto/env/v2/homedir_ios.go
+++
@@ -1,8 +0,0 @@
-//go:build ios
-
-package env
-
-// userHomeDir returns the same value as os.UserHomeDir for iOS
-func userHomeDir() (string, error) {
-	return "/", nil
-}
